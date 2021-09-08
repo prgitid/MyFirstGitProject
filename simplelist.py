cf6b7b24a0386972eb0301547b18242bf8728a6d
prgitid <priyesh_pr@yahoo.com>
2021-09-08 17:00:01 +0530

    print with append and insert in the list..

--- simplelist.py
+++ simplelist.py
@@ -2,8 +2,11 @@ fruits = ["banana","apple","mango"]
 #print(fruits)
 #print(fruits[0])
 #print(fruits[1])
-for i in fruits:
-    print(i)
-
+#for i in fruits:
+    #print(i)
+fruits.append("orange")
+print(fruits)
+fruits.insert(1,"Cherry")
+print(fruits)
 
 
