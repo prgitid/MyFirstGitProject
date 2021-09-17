b3634b880cde439258c712f8238d86f438b6f35f
prgitid <priyesh_pr@yahoo.com>
2021-09-17 10:48:11 +0530

    Reading list of Hostnames from hosts1.txt and printing ping response in different lines to file with datetime stamp with str...

--- simpleping.py
+++ simpleping.py
@@ -1,7 +1,9 @@
 #Reading list of Hostnames from hosts1.txt and printing ping response in different lines to hoststatus.txt
 import os
+import datetime
+today = datetime.datetime.today()
 filename = open('hosts1.txt','r')
-f = open('hoststatus.txt','w')
+f = open('hoststatus_'+str(today)+'.txt','w')
 lines = filename.readlines()
 for line in lines:
     response = os.system("ping -c 1 "+ line)
