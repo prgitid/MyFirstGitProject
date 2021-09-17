c8d31fe4600a68f4d54cdbb04b3f8152097ca0ff
prgitid <priyesh_pr@yahoo.com>
2021-09-17 23:41:41 +0530

    Program for ping the hosts from hosts1.txt with subprocess and storing the ping result to hoststatus.txt.

--- simpleping.py
+++ simpleping.py
@@ -2,6 +2,7 @@
 #Program for pinging the hosts from file hosts1.txt
 import subprocess
 filename = open('hosts1.txt','r')
+f = open('hoststatus.txt','w')
 lines = filename.readlines()
 for line in lines:
     response = subprocess.Popen(["ping","-c","1",line.rstrip()],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
@@ -9,6 +10,9 @@ for line in lines:
 #print(stdout)
 #print(stderr)
     if (response.returncode==0):
-        status = print(line.rstrip() + " is reachable")
+        status = line.rstrip() + " is reachable"
     else:
-        status = print(line.rstrip() + " is not reachable")
+        status = line.rstrip() + " is not reachable"
+    print(status)
+    f.writelines(status + '\n')
+f.close()
