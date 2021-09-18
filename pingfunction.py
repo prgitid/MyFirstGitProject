d0d8dd41618a146870636cb6a5feea2a3d2f2fbb
prgitid <priyesh_pr@yahoo.com>
2021-09-18 16:39:15 +0530

    Program for ping the range of ips with function and subprocess Popen..

--- pingfunction.py
+++ pingfunction.py
@@ -1,19 +1,12 @@
 import subprocess
 # defining ips in range of 192.168.1.1 to 1.10
-ips = ["192.168.1.{}".format(i) for i in range(1,11)]
+ips = ["192.168.0.{}".format(i) for i in range(1,11)]
 def ping(host):
     #Using subprocess run
-    ping_reply= subprocess.run(["ping","-c","1",host],stderr=subprocess.PIPE,stdout=subprocess.PIPE)
-    result = ""
-    if ping_reply.returncode==0:
-        # For Unreachable case returncode is also 0 , hence no response from host added
-        if "unreachable" in str(ping_reply.stdout):
-            result= ("\n* No response from host %s"% host)
-        else:
-            result= ("\n* %s is Reachable"% host)
-    elif ping_reply.returncode!=0:
-        result= ("\n %s is not Reachable"% host)
-    return result
+    ping_reply= subprocess.Popen(["ping","-c","1",host],stderr=subprocess.PIPE,stdout=subprocess.PIPE,text=True)
+    print(ping_reply.returncode)
+    print(ping_reply.communicate()[0])
+
 for ip in ips:
     print(ping(ip))
 
