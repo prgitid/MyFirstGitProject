a756bfe6773084f1c24e3cdb8f54de87177cfef7
prgitid <priyesh_pr@yahoo.com>
2021-09-22 13:01:03 +0530

    Writing the output of cmd to a text file using stdout = filename...

--- subprocessexamples.py
+++ subprocessexamples.py
@@ -14,13 +14,13 @@ import subprocess
 # With capture_output = True, p2.stdout.decode will show the output to console in text form.
 # With capture_output = True & text = True, p1.stdout will show the output in text or string form.
 # stdout=subprocess.PIPE will perform same function of capture_output=True
-p3 = subprocess.run(['ls','-al'],stdout=subprocess.PIPE,text=True)
+#p3 = subprocess.run(['ls','-al'],stdout=subprocess.PIPE,text=True)
 #Without print p2.stdout, no output is shown in console.
-print(p3.stdout)
+#print(p3.stdout)
 # Writing the output of cmd to to text file..
-#with open('output.txt','w') as newfile:
- #   p3=subprocess.run(['ls','-al'],stdout=newfile,text=True)
-  #  newfile.close()
+with open('output.txt','w') as newfile:
+    p3=subprocess.run(['ls','-al'],stdout=newfile,text=True)
+    newfile.close()
 # Capture Error output if dir doesn't exist but no output to console
 #p4 = subprocess.run(['ls','-al','dne'],capture_output=True,text=True)
 # capture returncode as 1 if error is ther.
