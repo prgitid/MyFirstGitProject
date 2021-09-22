b95ab533ace8fc50fcaa956f902d0d4103dbbd95
prgitid <priyesh_pr@yahoo.com>
2021-09-22 13:06:37 +0530

    To capture error returncode as 1 if non-existable argument are executed.

--- subprocessexamples.py
+++ subprocessexamples.py
@@ -24,9 +24,9 @@ import subprocess
 # Capture Error output if dir doesn't exist but no output to console
 p4 = subprocess.run(['ls','-al','dne'],capture_output=True,text=True)
 #print stderr output to console..
-print(p4.stderr)
-# capture returncode as 1 if error exist
-#print(p4.returncode)
+#print(p4.stderr)
+#capture returncode as 1 if error exist
+print(p4.returncode)
 #capture error in console.
 #print(p4.stderr)
 #With check=True, python will throw exception error in console.
