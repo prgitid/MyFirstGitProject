445ddf4d9940110a9c8a915cf47c439f474b1c41
prgitid <priyesh_pr@yahoo.com>
2021-09-22 12:32:37 +0530

    Program for read the hosts1.txt file , ping to each servcer with subprocess and function.xt

---
+++ subprocessexamples.py
@@ -0,0 +1,27 @@
+import subprocess
+
+p1 = subprocess.run(['ls','-al'])
+#Print the output of ls-al in the console with args and returncode status.
+print(p1)
+#print(p1.args[0])
+#print(p1.returncode)
+# With Capture_output = True & No p1.stdout ,will not show any output to console.
+#With capture_output = True & p1.stdout will show out in binary.
+# With capture_output = True, p1.stdout.decode will show the output to console in text form.
+# With capture_output = True & text = True, p1.stdout will show the output in text or string form.
+# stdout=subprocess.PIPE will perform same function of capture_output=True
+#p2 = subprocess.run(['ls','-al'],stdout=subprocess.PIPE,text=True)
+#Without print p2.stdout, no output is shown in console.
+#print(p2.stdout)
+# Writing the output of cmd to to text file..
+#with open('output.txt','w') as newfile:
+ #   p3=subprocess.run(['ls','-al'],stdout=newfile,text=True)
+  #  newfile.close()
+# Capture Error output if dir doesn't exist but no output to console
+#p4 = subprocess.run(['ls','-al','dne'],capture_output=True,text=True)
+# capture returncode as 1 if error is ther.
+#print(p4.returncode)
+#capture error in console.
+#print(p4.stderr)
+#With check=True, python will throw exception error in console.
+#p5 = subprocess.run(['ls','-al','dne'],capture_output=True,text=True,check=True)
